db0db2c15b3e3da9277e751cd468f1bea548e249
pablohc9 <62286348+pablohc9@users.noreply.github.com>
2021-05-11 17:15:42 +0200

    Path actualizada

--- path.php
+++ path.php
@@ -9,5 +9,9 @@
 	** través de hipervínculos, creamos la constante ROOT_PATH
 	** para no liarnos con las rutas relativas. 
 	*/
-	define("BASE_URL", "http://localhost:3000");
+	// LOCAL
+	// define("BASE_URL", "http://localhost:3000");
+
+	// DEPLOYED
+	define("BASE_URL", "https://pabloherranzcano.herokuapp.com");
 ?>
