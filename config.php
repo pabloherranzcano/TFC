0cecfad292b6a81acd09a96d3875f04dc1ade1d2
pablohc9 <62286348+pablohc9@users.noreply.github.com>
2021-05-18 21:36:11 +0200

    Prueba a heroku

--- config.php
+++ config.php
@@ -5,7 +5,7 @@
 /*******************************************************/
 
 // En local. Cambiar el número de puerto en caso necesario.
-// $path = "http://localhost:3000";
+$path = "http://localhost:3000";
 
 // Deplyed en heroku.
 $path = "https://pabloherranzcano.herokuapp.com";
@@ -21,10 +21,10 @@ $user = 'root';
 $pass = '';
 $db_name = 'tfcblog';
 
-// /* DEPLOYED */
-// // $host = 'remotemysql.com';
-// // $user = 'O4JnLPhRnn';
-// // $pass = 'YbASczJrKQ';
-// // $db_name = 'O4JnLPhRnn';
+/* DEPLOYED */
+$host = 'remotemysql.com';
+$user = 'O4JnLPhRnn';
+$pass = 'YbASczJrKQ';
+$db_name = 'O4JnLPhRnn';
 
 ?>
